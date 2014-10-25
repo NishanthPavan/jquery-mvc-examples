3a152b60d9063127c441347a7e9e31776b27d37c
micke239 <micke239@gmail.com>
2014-10-25 21:28:44 +0200

    removed unused code

--- admin-jobad/events.js
+++
@@ -1,9 +0,0 @@
-/**
- * jQuery allows event binding on objects.
- * This module creates an empty objects and wraps it with jquery,
- * allowing us to define global application events
- */
-
-define(['jquery'], function($) {
-    return $({});
-});
